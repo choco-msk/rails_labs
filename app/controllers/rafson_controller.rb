65307c77dd5eb8c48f5bbe5ce0c778379c3e1958
choco-msk <temerfromkazan@mail.ru>
2020-12-03 01:44:16 +0300

    test

---
+++ app/controllers/rafson_controller.rb
@@ -0,0 +1,20 @@
+class RafsonController < ApplicationController
+  attr_reader :result
+
+  def input; end
+
+  def view
+    eps = 0.001
+    if params[:num].to_s[/^\d+$/]
+        num = params[:num].to_f
+        num_new = num
+        @result = []
+        until ((num_new**2 - num)/num) < eps do
+          num_new = 0.5*(num_new + num/num_new)
+          @result << num_new
+        end
+    else
+      @result = "Error"
+    end
+  end
+end
---
+++ app/helpers/rafson_helper.rb
@@ -0,0 +1,2 @@
+module RafsonHelper
+end
