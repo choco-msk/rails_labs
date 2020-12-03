336b7271c903b29cff48362de07d91c1af883140
choco-msk <temerfromkazan@mail.ru>
2020-12-03 19:13:21 +0300

    js_file

--- app/javascript/packs/input.js
+++ app/javascript/packs/input.js
@@ -1,11 +1,17 @@
 function handleResult(event) {
     const [data] = event.detail
     var result = document.getElementById("result");
-    result.innerHTML = "";
-    data.value.forEach(function (item, i, arr) {
-        result.innerHTML += "<tr><td>" + i + "</td><td>" + data.value[i]
-            + "</td></tr>";
-    })
+    if (data.value != "Error") {
+        result.innerHTML = "";
+        data.value.forEach(function (item, i, arr) {
+            result.innerHTML += "<tr><td>" + (i+1) + "</td><td>" + data.value[i]
+                + "</td></tr>";
+        })
+    }
+    else {
+        result.innerHTML = "";
+        result.innerHTML += "<tr><td>Error</td><tr>";
+    }
 }
 
 document.addEventListener('turbolinks:load', () => {
