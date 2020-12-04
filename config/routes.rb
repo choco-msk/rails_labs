9fc4471ce6090045f06b9de99d9efca8778b5de1
choco-msk <temerfromkazan@mail.ru>
2020-12-04 15:30:37 +0300

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'rafson/input'
   get 'rafson/view'
+  root 'rafson#input'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/rspec/UntitledTestCase.rb
@@ -0,0 +1,63 @@
+require "json"
+require "selenium-webdriver"
+require "rspec"
+include RSpec::Expectations
+
+describe "UntitledTestCase" do
+
+  before(:each) do
+    @driver = Selenium::WebDriver.for :firefox
+    @base_url = "https://www.google.com/"
+    @accept_next_alert = true
+    @driver.manage.timeouts.implicit_wait = 30
+    @verification_errors = []
+  end
+
+  after(:each) do
+    @driver.quit
+    @verification_errors.should == []
+  end
+
+  it "test_untitled_test_case" do
+    @driver.get "http://127.0.0.1:3000/"
+    @driver.find_element(:id, "num").click
+    @driver.find_element(:id, "num").clear
+    @driver.find_element(:id, "num").send_keys "3"
+    sleep(2)
+    @driver.find_element(:name, "commit").click
+    (@driver.find_element(:id, "result").text).should == "1 2\n2 1.75\n3 1.7321428571428572"
+  end
+
+  def element_present?(how, what)
+    @driver.find_element(how, what)
+    true
+  rescue Selenium::WebDriver::Error::NoSuchElementError
+    false
+  end
+
+  def alert_present?()
+    @driver.switch_to.alert
+    true
+  rescue Selenium::WebDriver::Error::NoAlertPresentError
+    false
+  end
+
+  def verify(&blk)
+    yield
+  rescue ExpectationNotMetError => ex
+    @verification_errors << ex
+  end
+
+  def close_alert_and_get_its_text(how, what)
+    alert = @driver.switch_to().alert()
+    alert_text = alert.text
+    if (@accept_next_alert) then
+      alert.accept()
+    else
+      alert.dismiss()
+    end
+    alert_text
+  ensure
+    @accept_next_alert = true
+  end
+end
