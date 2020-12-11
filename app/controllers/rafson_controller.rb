2e2df612d87a3afb166c58c1d72d189a8bed3885
choco-msk <temerfromkazan@mail.ru>
2020-12-11 18:26:32 +0300

    test

--- app/controllers/rafson_controller.rb
+++ app/controllers/rafson_controller.rb
@@ -4,19 +4,46 @@ class RafsonController < ApplicationController
   def input; end
 
   def view
-    if params[:num]
-    eps = 0.001
-    if params[:num].to_s[/^\d+$/]
-        num = params[:num].to_f
-        num_new = num
-        @result = []
-        until ((num_new**2 - num)/num) < eps do
-          num_new = 0.5*(num_new + num/num_new)
-          @result << num_new
-        end
+    num = params[:num]
+    if (num.to_s[/^\d+$/]) && (num.to_f.positive?)
+      if (res = Result.find_by_value(num))
+        @result = ActiveSupport::JSON.decode(res.result)
+      else
+        @result = find_square(num.to_f)
+        res = Result.create value: num, result: ActiveSupport::JSON.encode(@result)
+        res.save
+      end
     else
       @result = "Error"
     end
+
+    respond_to do |format|
+      format.html
+      format.json do
+        render json:
+                   { type_result: @result.class.to_s, value_result: @result }
+      end
+    end
+
   end
+
+  def find_square(num)
+    eps = 0.001
+    num_new = num
+    res = []
+    until ((num_new**2 - num.to_f)/num.to_f) < eps do
+      num_new = 0.5*(num_new + num.to_f/num_new)
+      res << num_new
+    end
+    return res
   end
+
+  def results
+    result = Result.all.map { |el| { value: el.value, result: ActiveSupport::JSON.decode(el.result) } }
+
+    respond_to do |format|
+      format.xml { render xml: result.to_xml }
+    end
+  end
+
 end
--- test/controllers/rafson_controller_test.rb
+++ test/controllers/rafson_controller_test.rb
@@ -1,17 +1,34 @@
 require 'test_helper'
 
 class RafsonControllerTest < ActionDispatch::IntegrationTest
-  test "should get input" do
-    get rafson_input_url
+  test 'test_return_json' do
+    get rafson_view_url, params: { num: 100, format: 'json' }
     assert_response :success
+    assert_includes @response.headers['Content-Type'], 'application/json'
   end
-  test "should get incorrect" do
-    get rafson_view_url, params: {num: "fggf"}
-    assert_equal assigns[:result], "Error"
+  test 'test_record_in_db' do
+    before = Result.count
+    get rafson_view_url, params: { num: 333 }
+    after = Result.count
+
+    assert_equal before + 1, after
   end
-  test "should get correct" do
-    get rafson_view_url, params: {num: "16"}
-    res = assigns[:result][-1]
-    assert_in_delta res,4,0.001
+
+  test 'test_different_response_json' do
+    get rafson_view_url, params: { num: 10, format: 'json' }
+    first = JSON.parse @response.body
+
+    get rafson_view_url, params: { num: 20, format: 'json' }
+    second = JSON.parse @response.body
+
+    refute_equal first, second
+  end
+
+  test 'test_different_response' do
+    get rafson_view_url, params: { num: 30 }
+    first = assigns[:result]
+    get rafson_view_url, params: { num: 40 }
+    second = assigns[:result]
+    refute_equal first, second
   end
 end
